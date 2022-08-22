95b13ad2bfae9a2739d43114c9fab64d475f037f
Amith Kumar Manjunath <ammanjunath@c02dv0ylmd6t.lan>
2022-08-22 12:19:11 -0400

    test1-demo

---
+++ migrations/V000006__populate_all_order_events_test1.sql
@@ -0,0 +1,12 @@
+CREATE STREAM IF NOT EXISTS  ALL_ORDER_EVENTS_TEST1 (
+  orderid STRING, 
+  name STRING, 
+  items ARRAY<STRING>,
+  cost DECIMAL(6, 2),
+  ordertime TIMESTAMP,
+  purchase_method STRING
+) WITH (
+  kafka_topic='all_order_events', 
+  partitions=1,
+  value_format='json'
+);
